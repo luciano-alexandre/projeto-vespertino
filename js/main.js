558880028146e8ca49c6ba1e208feb2276f10fd7
Luciano Alexandre de Farias Silva <luciano.cnrn@gmail.com>
2022-12-02 09:52:49 -0300

    Refatoração da página inicial
    adição de conteúdo da página index.html

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
 function login()
 {
-    const username = "luciano";
-    const password = "senha123";
+    const login_correto = "luciano";
+    const password_correto = "senha123";
     
-    let usuario_informado 
-        = document.getElementById("username").value;
-    
-    let senha_informada
-        = document.getElementById("password").value;
-    
-    if(usuario_informado == username
-            && senha_informada == password){
+    let login_informado = document.getElementById("username").value;
+    let password_informado = document.getElementById("password").value;
 
+    if(login_informado == login_correto 
+        && password_informado == password_correto)
+    {
         let continuar_logado 
-            = document.getElementById("remember-me");
-         
+                = document.getElementById("remember-me");
+        
         if(continuar_logado){
             document.cookie = "logado=1";
-        }   
-        
-        window.location.href = "index.html";        
-    } else {
-        alert("Login ou Senha inválidas");       
-    }    
+        }
+
+        window.location.href = "index.html";
+    } else{
+        alert("Login ou Senha incorretos");
+    }
 }
 
-function register(){
-    let username 
-        = document.getElementById("username").value;
-    let email 
-        = document.getElementById("email").value;
-    let password 
-        = document.getElementById("password").value;
+function register()
+{
+    let username = document.getElementById("username").value;   
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
     let repeat_password 
-        = document.getElementById("repeat-password").value;    
-    
-    if(password != repeat_password){
-        alert("Você deve digitar duas senhas iguais.");
-    }    
-}
+        = document.getElementById("repeat-password").value;
 
+    if(password != repeat_password){
+        alert("Digite as senhas iguais...");
+    } else {
+        const user = {
+            username: username,
+            password: password,
+            email: email
+          };
+
+        alert(user.username + ", seu cadastro foi realizado com sucesso.");
+        window.location.href = "index.html";
+    }
+}
